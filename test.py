eba87dab5bb6ecc36a955675ac43c0506a5c024d
aymen6524 <laouar.abed@univ-guelma.dz>
2024-11-16 22:01:45 +0100

    interface

--- test.py
+++ test.py
@@ -1,37 +1,9 @@
-# Define facts about the shape
 import sys
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont, QColor
 from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QLabel, QVBoxLayout, QLineEdit, QPushButton, QTextEdit, QWidget, QFrame
 )
-facts = {
-    "Triangle": {"sides": 3, "sum_of_angles": 180},
-    "Pentagon": {"sides": 5, "sum_of_angles": 540} # 5 sides AND sum of angles = 540
-}
-
-# Define rule ofunction t check if a shape is a triangle
-def is_triangle(properties):
-    return properties.get("sides") == 3 and properties.get("sum_of_angles") == 180
-def is_pentagon(properties):
-    return properties.get("sides") == 5 and properties.get("sum_of_angles") == 540
-# List of rules with only the triangle rule
-rules = [
-    ("is_triangle", is_triangle),
-    ("is_pentagon", is_pentagon)
-]
-
-# Forward chaining: Apply the rule to check if the shape is a triangle
-for shape, properties in facts.items():
-    for rule_name, rule_function in rules:
-        if rule_function(properties):
-            properties[rule_name] = True  # Mark the shape as matching the triangle rule
-
-# Display result for Triangle
-#for shape, properties in facts.items():
- #   print(f"{shape} is a Triangle: {properties.get('is_triangle')}")
-  #  print(f"{shape} is a Pentagon: {properties.get('is_pentagon')}")
- 
 
 
 class ShapeIdentifierApp(QMainWindow):
